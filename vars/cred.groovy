4c516eeff6e5a215fc6500df9cfbe35d0ccdd59f
pru24-git <pruaragit@gmail.com>
2026-07-08 15:57:31 +0530

    Update cred.groovy

--- vars/cred.groovy
+++ vars/cred.groovy
@@ -2,5 +2,5 @@ def call(String username, String image, int tag_number){
   withCredentials([string(credentialsId: 'docker_pass', variable: 'dh_password')]) {
   sh "docker image tag ${image} ${username}/${image}:${tag_number}"
   sh "docker login -u ${username} -p $dh_password "
-  sh "docker push ${username}/${image}:${tag_number}
+  sh "docker push ${username}/${image}:${tag_number}"
 }
